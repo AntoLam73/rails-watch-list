6b06ddc646f265edde69414b19b04a88e49d84cf
Antonius Lambsdorff <lambsdorffa@gmail.com>
2025-11-20 17:18:37 +0100

    Small update, add is still not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
-  resources :lists do
-    resources :bookmarks, only: [:new, :create, :destroy]
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: :destroy
 
   get "up" => "rails/health#show", as: :rails_health_check
 
