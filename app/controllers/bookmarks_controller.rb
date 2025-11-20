537c584f35cda3f3988162fb10fad49a43787068
Antonius Lambsdorff <lambsdorffa@gmail.com>
2025-11-20 17:05:29 +0100

    Add a new movie to be done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new
+    end
+  end
+
+  def delete
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,49 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Seed real movies by using this API (with open-uri and json ruby libs): "https://tmdb.lewagon.com/movie/top_rated"
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning up database...'
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts 'Creating movies...'
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+movies['results'].each do |movie|
+  Movie.create!(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts "Created #{Movie.count} movies."
+
+
+puts 'Creating lists...'
+list_names = ['Classic movies', 'Superhero']
+list_names.each do |name|
+  List.create!(name: name)
+end
+
+puts "Created #{List.count} lists."
+
+
+# Associating movies to lists with bookmarks
+puts 'Creating bookmarks...'
+List.all.each do |list|
+  movies_sample = Movie.order('RANDOM()').limit(5)
+  movies_sample.each do |movie|
+    Bookmark.create!(
+      list: list,
+      movie: movie,
+      comment: "Great movie!"
+    )
+  end
+end
+puts "Created #{Bookmark.count} bookmarks."
+
+puts 'Finished!'
