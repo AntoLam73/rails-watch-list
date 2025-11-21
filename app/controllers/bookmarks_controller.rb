b72bc2b3f9ed18937ac72bd86408e4692624d465
Antonius Lambsdorff <lambsdorffa@gmail.com>
2025-11-21 12:32:57 +0100

    Got it working

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,23 +6,26 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@bookmark.list)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def delete
+  def destroy
     @bookmark = Bookmark.find(params[:id])
+    list = @bookmark.list
     @bookmark.destroy
-    redirect_to bookmarks_path
+    redirect_to list_path(list), notice: 'Bookmark was successfully deleted.'
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
